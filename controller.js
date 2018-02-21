757a8c22c8b82698cda5400cc8f88545f5b25cc7
Jake Higgs <jakehiggs@Jakes-MacBook-Pro.local>
2018-02-20 20:23:37 -0700

    done

---
+++ controller.js
@@ -0,0 +1,10 @@
+module.exports = {
+    getPlanes: (req, res, next)=>{
+        const dbInstance = req.app.get('db');
+        dbInstance.getPlanes([25])
+        .then(planes=> {res.status(200).send(planes); })
+        .catch(err=> res.status(500).send(err))
+
+     }
+
+}
